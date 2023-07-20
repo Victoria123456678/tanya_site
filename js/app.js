25e25a0b97dfc62cf545c72e0d01432a8f503990
Victoria123456678 <140052151+Victoria123456678@users.noreply.github.com>
2023-07-20 12:58:02 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,34 +1,33 @@
 window.addEventListener('load', async () => {
-  if ('serviceWorker' in navigator) {
-    try {
-      const reg = await navigator.serviceWorker.register('/sw.js')
-      console.log('Service worker register success', reg)
-    } catch (e) {
-      console.log('Service worker register fail')
-    }
-  }
+if ('serviceWorker' in navigator) {
+try {
+const reg = await navigator.serviceWorker.register('/sw.js')
+console.log('Service worker register success', reg)
+} catch (e) {
+console.log('Service worker register fail')
+}
+}
 
-  await loadPosts()
+await loadPosts()
 })
 
-
 async function loadPosts() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=11')
-  const data = await res.json()
+const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=11')
+const data = await res.json()
 
-  const container = document.querySelector('#posts')
-  container.innerHTML = data.map(toCard).join('\n')
+const container = document.querySelector('#posts')
+container.innerHTML = data.map(toCard).join('\n')
 }
 
 function toCard(post) {
-  return `
-    <div class="card">
-      <div class="card-title">
-        ${post.title}
-      </div>
-      <div class="card-body">
-        ${post.body}
-      </div>
-    </div>
-  `
+return `
+<div class="card">
+<div class="card-title">
+${post.title}
+</div>
+<div class="card-body">
+${post.body}
+</div>
+</div>
+`
 }
